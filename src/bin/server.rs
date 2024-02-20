0e5861bd3e68043bafc14356cd6f9fe8dce313cf
yamelsenih <ysenih@erpya.com>
2024-02-20 12:14:23 -0400

    Add docs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,6 @@
 use std::env;
 use dotenv::dotenv;
-use s3_gateway_rs::controller::s3::{delete_object, request_signed_url, get_valid_file_name};
+use s3_gateway_rs::controller::s3::{delete_object, get_list_objects, get_valid_file_name, request_signed_url};
 use salvo::{prelude::*, cors::Cors, hyper::Method};
 extern crate serde_json;
 use simple_logger::SimpleLogger;
@@ -79,7 +79,7 @@ async fn main() {
                 )
                 .push(
                     Router::with_path("resources")
-                        .get(get_presigned_url_download_file)
+                        .get(get_resources_file_container_based)
                 )
         )
     ;
@@ -166,6 +166,29 @@ async fn get_presigned_url_put_file<'a>(_req: &mut Request, _res: &mut Response)
 }
 
 #[handler]
+async fn get_resources_file_container_based<'a>(_req: &mut Request, _res: &mut Response) {
+    let _client_id = _req.param::<String>("client_id");
+    let _container_id = _req.param::<String>("container_id");
+    let _file_name = _req.param::<String>("file_name");
+    let _container_type = _req.query::<String>("container_type");
+    let _table_name = _req.query::<String>("table_name");
+    let _column_name = _req.query::<String>("column_name");
+    let _record_id = _req.query::<String>("record_id");
+    let _user_id = _req.query::<String>("user_id");
+    let _seconds = _req.query::<u32>("seconds");
+    let _object_list = get_list_objects(_client_id.to_owned(), _container_id.to_owned(), _container_type.to_owned(), _table_name.to_owned(), _column_name.to_owned(), _record_id.to_owned(), _user_id.to_owned()).await;
+    match _object_list {
+        Ok(_objects) => {
+           _res.render(Json(_objects))
+        },
+        Err(error) => {
+            _res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
+            _res.render(Json(error.to_string()));
+        }
+    }
+}
+
+#[handler]
 async fn get_presigned_url_put_file_container_based<'a>(_req: &mut Request, _res: &mut Response) {
     let _client_id = _req.param::<String>("client_id");
     let _container_id = _req.param::<String>("container_id");
@@ -176,6 +199,7 @@ async fn get_presigned_url_put_file_container_based<'a>(_req: &mut Request, _res
     let _record_id = _req.query::<String>("record_id");
     let _user_id = _req.query::<String>("user_id");
     let _seconds = _req.query::<u32>("seconds");
+    let _ = get_list_objects(_client_id.to_owned(), _container_id.to_owned(), _container_type.to_owned(), _table_name.to_owned(), _column_name.to_owned(), _record_id.to_owned(), _user_id.to_owned()).await;
     //  Get Valid File Name
     let _file_name_to_store = get_valid_file_name(_client_id, _container_id, _file_name, _container_type, _table_name, _column_name, _record_id, _user_id);
     match _file_name_to_store {
--- src/controller/s3.rs
+++ src/controller/s3.rs
@@ -1,12 +1,15 @@
+use std::collections::HashMap;
 use std::{env, io::Error, io::ErrorKind};
 use std::str::FromStr;
 
 use http::Method;
-use minio::s3::args::{GetPresignedObjectUrlArgs, RemoveObjectArgs};
+use minio::s3::args::{GetPresignedObjectUrlArgs, ListObjectsV2Args, RemoveObjectArgs};
 use minio::s3::client::Client;
 use minio::s3::creds::StaticProvider;
 use minio::s3::http::BaseUrl;
+use minio::s3::response::ListObjectsV2Response;
 use regex::Regex;
+use serde::Serialize;
 
 fn get_valid_path(_client_id: Option<String>, _container_id: Option<String>, _container_type: Option<String>, _table_name: Option<String>, _column_name: Option<String>, _record_id: Option<String>, _user_id: Option<String>, _include_user: bool) -> Result<String, std::io::Error> {
     if _client_id.to_owned().is_none() {
@@ -85,6 +88,108 @@ pub fn get_valid_file_name(_client_id: Option<String>, _container_id: Option<Str
         Err(error) => Err(Error::new(ErrorKind::InvalidData.into(), error))
     }
 }
+
+pub async fn get_list_objects(_client_id: Option<String>, _container_id: Option<String>, _container_type: Option<String>, _table_name: Option<String>, _column_name: Option<String>, _record_id: Option<String>, _user_id: Option<String>) -> Result<ResourceResponse, std::io::Error> {
+    let _s3_url =  match env::var("S3_URL") {
+        Ok(value) => value,
+        Err(_) => {
+            log::info!("Variable `S3_URL` Not found");
+            "".to_owned()
+        }.to_owned(),
+    };
+    let _bucket_name =  match env::var("BUCKET_NAME") {
+        Ok(value) => value,
+        Err(_) => {
+            log::info!("Variable `BUCKET_NAME` Not found");
+            "".to_owned()
+        }.to_owned(),
+    };
+    let _api_key =  match env::var("API_KEY") {
+        Ok(value) => value,
+        Err(_) => {
+            log::info!("Variable `API_KEY` Not found");
+            "".to_owned()
+        }.to_owned(),
+    };
+    let _secret_key =  match env::var("SECRET_KEY") {
+        Ok(value) => value,
+        Err(_) => {
+            log::info!("Variable `SECRET_KEY` Not found");
+            "".to_owned()
+        }.to_owned(),
+    };
+    let mut _base_url: BaseUrl = BaseUrl::from_str(&_s3_url).expect("Error with url");
+    _base_url.https = false;
+
+    let static_provider: StaticProvider = StaticProvider::new(
+        &_api_key, 
+        &_secret_key, None);
+
+    let client = Client::new(_base_url.clone(), Some(Box::new(static_provider)), None, None).unwrap();
+    let _value = get_valid_path(_client_id, _container_id, _container_type, _table_name, _column_name, _record_id, _user_id, true);
+    let _prefix = match _value {
+        Ok(_folder_name) => Some(_folder_name),
+        Err(error) => {
+            log::warn!("Error Getting path {:?}", error);
+            Some("".to_owned())
+        }
+    };
+    let mut _args = ListObjectsV2Args::new(&_bucket_name).unwrap();
+    _args.prefix = _prefix.as_deref();
+    match client
+            .list_objects_v2(&_args)
+            .await {
+        Ok(value) => Ok(ResourceResponse::new(value)),
+        Err(error) => Err(Error::new(ErrorKind::InvalidData.into(), error)),
+    }
+    
+}
+
+#[derive(Serialize, Debug, Clone)]
+pub struct ResourceResponse {
+    pub parent_folder: Option<String>,
+    pub resources: Option<Vec<Resource>>
+}
+
+#[derive(Serialize, Debug, Clone)]
+pub struct Resource {
+    pub name: String,
+    // pub last_modified: Option<UtcTime>,
+    pub etag: Option<String>, // except DeleteMarker
+    pub owner_name: Option<String>,
+    pub size: Option<usize>, // except DeleteMarker
+    pub storage_class: Option<String>,
+    pub is_latest: bool,            // except ListObjects V1/V2
+    pub version_id: Option<String>, // except ListObjects V1/V2
+    pub user_metadata: Option<HashMap<String, String>>,
+    pub is_prefix: bool,
+    pub is_delete_marker: bool,
+    pub encoding_type: Option<String>,
+}
+
+impl ResourceResponse {
+    pub fn new(_data: ListObjectsV2Response) -> Self {
+        ResourceResponse {
+            parent_folder: _data.prefix,
+            resources: Some(_data.contents.iter().map(|_content| {
+                Resource { 
+                    name: _content.to_owned().name, 
+                    etag: _content.to_owned().etag, 
+                    owner_name: _content.to_owned().owner_name, 
+                    size: _content.size, 
+                    storage_class: _content.to_owned().storage_class, 
+                    is_latest: _content.is_latest, 
+                    version_id: _content.to_owned().version_id, 
+                    user_metadata: _content.to_owned().user_metadata,
+                    is_prefix: _content.is_prefix, 
+                    is_delete_marker: _content.is_delete_marker, 
+                    encoding_type: _content.to_owned().encoding_type 
+                }
+            }).collect::<Vec<Resource>>()),
+        }
+    }
+}
+
 
 pub async fn request_signed_url(_file_name: String, _method: Method, _seconds: Option<u32>) -> Result<String, std::io::Error> {
     let _s3_url =  match env::var("S3_URL") {
@@ -201,3 +306,69 @@ pub async fn delete_object(_file_name: String) -> Result<(), std::io::Error> {
         Err(error) => return Err(Error::new(ErrorKind::InvalidData.into(), error))
     }
 }
+
+// pub async fn ge_objects_list(_folder_name: String, _method: Method, _seconds: Option<u32>) -> Result<String, std::io::Error> {
+//     let _s3_url =  match env::var("S3_URL") {
+//         Ok(value) => value,
+//         Err(_) => {
+//             log::info!("Variable `S3_URL` Not found");
+//             "".to_owned()
+//         }.to_owned(),
+//     };
+//     let _bucket_name =  match env::var("BUCKET_NAME") {
+//         Ok(value) => value,
+//         Err(_) => {
+//             log::info!("Variable `BUCKET_NAME` Not found");
+//             "".to_owned()
+//         }.to_owned(),
+//     };
+//     let _api_key =  match env::var("API_KEY") {
+//         Ok(value) => value,
+//         Err(_) => {
+//             log::info!("Variable `API_KEY` Not found");
+//             "".to_owned()
+//         }.to_owned(),
+//     };
+//     let _secret_key =  match env::var("SECRET_KEY") {
+//         Ok(value) => value,
+//         Err(_) => {
+//             log::info!("Variable `SECRET_KEY` Not found");
+//             "".to_owned()
+//         }.to_owned(),
+//     };
+//     let mut _base_url: BaseUrl = BaseUrl::from_str(&_s3_url).expect("Error with url");
+//     _base_url.https = false;
+
+//     let static_provider: StaticProvider = StaticProvider::new(
+//         &_api_key, 
+//         &_secret_key, None);
+
+//     let client = Client::new(_base_url.clone(), Some(Box::new(static_provider)), None, None).unwrap();
+
+//     let args_to_match = GetPresignedObjectUrlArgs::new(
+//         &_bucket_name,
+//         _folder_name.as_str(),
+//         _method,
+//     );
+
+//     let mut stream = client
+//             .list_objects(ListObjectsArgs::new(&self.test_bucket).unwrap())
+//             .await;
+
+//     match args_to_match {
+//         Ok(value) => {
+//             let mut _presigned_parameters = value;
+//             match _seconds {
+//                 Some(seconds) => _presigned_parameters.expiry_seconds = Some(seconds),
+//                 None => {
+
+//                 }
+//             };
+//             match client.get_presigned_object_url(&_presigned_parameters).await {
+//                 Ok(url) => Ok(url.url),
+//                 Err(error) => return Err(Error::new(ErrorKind::InvalidData.into(), error))
+//             }
+//         },
+//         Err(error) => return Err(Error::new(ErrorKind::InvalidData.into(), error))
+//     }
+// }
